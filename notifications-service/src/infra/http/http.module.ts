fa6107e27abb5e36a1e35c4fcbe7ec81ce604a30
Pedro Rodrigues <pedrodesarodrigues@gmail.com>
2022-12-16 23:39:24 -0300

    mappers + factory + finish app/infra implement.

--- notifications-service/src/infra/http/http.module.ts
+++ notifications-service/src/infra/http/http.module.ts
@@ -1,12 +1,28 @@
 import { Module } from "@nestjs/common";
-import { SendNotification } from "src/application/use-cases/send-notification";
 import { DatabaseModule } from "../database/database.module";
+
 import { NotificationsController } from "./controllers/notifications.controller";
+
+import { CancelNotification } from "@application/use-cases/cancel-notification";
+import { ReadNotification } from "@application/use-cases/read-notification";
+import { UnreadNotification } from "@application/use-cases/unread-notification";
+import { CountRecipientNotifications } from "@application/use-cases/count-recipient-notifications";
+import { GetRecipientNotifications } from "@application/use-cases/get-recipient-notifications";
+import { SendNotification } from "@application/use-cases/send-notification";
 
 @Module({
     imports: [DatabaseModule],
     controllers: [NotificationsController],
-    providers: [SendNotification]
+    providers: [
+        CancelNotification,
+        ReadNotification,
+        UnreadNotification,
+        CountRecipientNotifications,
+        GetRecipientNotifications,
+        SendNotification
+    ]
 })
+
+
 
 export class HttpModule { }
